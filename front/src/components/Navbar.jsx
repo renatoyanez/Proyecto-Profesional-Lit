712d4189f7b76c7ba98758abdb3379c80012470d
Rosanavpu <rosanap06@gmail.com>
2020-04-16 15:04:41 -0300

    estilo mejorado, falta por mejorar

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,61 +1,176 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
-const styles = {
-  navbar: {
-    height: "6vh"
-  },
-  userStyle: {
-    textAlign: "right"
-  }
-};
-export default ({ user, onLogout }) => {
-  console.log("USER: ", user);
-  
-  return (
-    <nav
-      style={styles.navbar}
-      className="navbar navbar-expand-md navbar-dark fixed-top bg-dark"
-    >
-      <Link to="/" className="navbar-brand">
-          L I T
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarCollapse"
-        aria-controls="navbarCollapse"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav mr-auto">
-        {user ? (
-          <li onClick={onLogout} className="nav-item">
-            <Link to="/login" className="nav-link">
-              Log-Out <span className="sr-only">(current)</span>
-            </Link>
-        </li>) : (
-          <>
-          <li className="nav-item">
-            <Link to="/login" className="nav-link">
-              Log-In <span className="sr-only">(current)</span>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/register" className="nav-link">
-              Sign-Up
-            </Link>
-          </li>
-          </>
-          )}
-          <li className="nav-item">
-              {user ? (`Welcome ${user}`) : (null)}
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+// import React, { Fragment } from "react";
+// import { Link } from "react-router-dom";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
+// import Button from "@material-ui/core/Button";
+// import { makeStyles } from "@material-ui/core/styles";
+// import clsx from "clsx";
+// import Typography from "@material-ui/core/Typography";
+
+// const drawerWidth = 240;
+
+// const useStyles = makeStyles(theme => ({
+//     root: {
+//       display: "flex"
+//     },
+//     toolbar: {
+//       paddingRight: 24 // keep right padding when drawer closed
+//     },
+//     toolbarIcon: {
+//       display: "flex",
+//       alignItems: "center",
+//       justifyContent: "flex-end",
+//       padding: "0 8px",
+//       ...theme.mixins.toolbar
+//     },
+//     appBar: {
+//       zIndex: theme.zIndex.drawer + 1,
+//       transition: theme.transitions.create(["width", "margin"], {
+//         easing: theme.transitions.easing.sharp,
+//         duration: theme.transitions.duration.leavingScreen
+//       })
+//     },
+//     appBarShift: {
+//       marginLeft: drawerWidth,
+//       width: `calc(100% - ${drawerWidth}px)`,
+//       transition: theme.transitions.create(["width", "margin"], {
+//         easing: theme.transitions.easing.sharp,
+//         duration: theme.transitions.duration.enteringScreen
+//       })
+//     },
+//     menuButton: {
+//       marginRight: 36
+//     },
+//     menuButtonHidden: {
+//       display: "none"
+//     },
+//     title: {
+//       flexGrow: 1
+//     },
+//     drawerPaper: {
+//       position: "relative",
+//       whiteSpace: "nowrap",
+//       width: drawerWidth,
+//       transition: theme.transitions.create("width", {
+//         easing: theme.transitions.easing.sharp,
+//         duration: theme.transitions.duration.enteringScreen
+//       })
+//     },
+//     drawerPaperClose: {
+//       overflowX: "hidden",
+//       transition: theme.transitions.create("width", {
+//         easing: theme.transitions.easing.sharp,
+//         duration: theme.transitions.duration.leavingScreen
+//       }),
+//       width: theme.spacing(7),
+//       [theme.breakpoints.up("sm")]: {
+//         width: theme.spacing(9)
+//       }
+//     },
+//     appBarSpacer: theme.mixins.toolbar,
+//     content: {
+//       flexGrow: 1,
+//       height: "100vh",
+//       overflow: "auto"
+//     },
+//     container: {
+//       paddingTop: theme.spacing(4),
+//       paddingBottom: theme.spacing(4)
+//     },
+//     paper: {
+//       padding: theme.spacing(2),
+//       display: "flex",
+//       overflow: "auto",
+//       flexDirection: "column"
+//     },
+//     fixedHeight: {
+//       height: 240
+//     }
+//   }));
+// export default ({ user, onLogout }) => {
+//     const classes = useStyles();
+//     const [open, setOpen] = React.useState(true);
+//     const handleClose = event => {
+//         if (anchorRef.current && anchorRef.current.contains(event.target)) {
+//           return;
+//         }
+    
+//         setOpen(false);
+//       };
+    
+//       const handleDrawerClose = () => {
+//         setOpen(true);
+//       };
+//   return (
+//     <AppBar
+//     position="absolute"
+//     className={clsx(classes.appBar, open && classes.appBarShift)}
+//   >
+//     <Toolbar className={classes.toolbar}>
+//       <IconButton
+//         edge="start"
+//         color="inherit"
+//         aria-label="open drawer"
+//         onClick={handleDrawerClose}
+//         className={clsx(
+//           classes.menuButton,
+//           open && classes.menuButtonHidden
+//         )}
+//       >
+//         <MenuIcon />
+//       </IconButton>
+//       <Link href="/"className="navbar-brand">
+//         <Typography
+//           component="h1"
+//           variant="h6"
+//           color="inherit"
+//           noWrap
+//           className={classes.title}
+//         >
+//           L I T
+//         </Typography>
+//       </Link>
+//       <IconButton color="inherit">
+//         {/* <Badge badgeContent={4} color="secondary"> */}
+//           <Link href="/login">
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               color="primary"
+//               className={classes.link}
+              
+//               // color="primary"
+//               // variant="outlined"
+//               // className={classes.link}
+//             >
+//               Login
+//             </Button>
+//           </Link>
+//         {/* </Badge> */}
+//       </IconButton>
+//       <IconButton color="inherit">
+//         {/* <Badge badgeContent={4} color="secondary"> */}
+//           <Link href="/register">
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               color="primary"
+//               className={classes.link}
+              
+//               // color="primary"
+//               // variant="outlined"
+//               // className={classes.link}
+//             >
+//               Registrate
+//             </Button>
+//           </Link>
+//         {/* </Badge> */}
+//       </IconButton>
+//     </Toolbar>
+//   </AppBar>
+//   );
+// };
--- front/src/containers/SidebarContainer.jsx
+++ front/src/containers/SidebarContainer.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import Sidebar from "../components/Sidebar";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { fetchFilteredProperties, fetchProducts } from "../redux/actions/getProperties";
+import {
+  fetchFilteredProperties,
+  fetchProducts
+} from "../redux/actions/getProperties";
+import { logOutCreator } from "../redux/actions/getUser"
 
 class SidebarContainer extends Component {
   constructor(props) {
@@ -14,6 +18,7 @@ class SidebarContainer extends Component {
     };
     this.onSearch = this.onSearch.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.onLogout = this.onLogout.bind(this);
   }
 
   handleChange(e) {
@@ -22,36 +27,31 @@ class SidebarContainer extends Component {
     });
   }
 
-/**** Para el search de propiedades por nombre, ubicacion. etc ****/
+  /**** Para el search de propiedades por nombre, ubicacion. etc ****/
   onSearch(event) {
     event.preventDefault();
-    const filterValue = this.state.menor || this.state.mayor ? true : false
-    this.props.fetchFilteredProperties(this.state.clearInput, { menor: this.state.menor,
-      mayor: this.state.mayor, filterByPrice: filterValue})
+    const filterValue = this.state.menor || this.state.mayor ? true : false;
+    this.props
+      .fetchFilteredProperties(this.state.clearInput, {
+        menor: this.state.menor,
+        mayor: this.state.mayor,
+        filterByPrice: filterValue
+      })
       .then(() => {
-          this.props.history.push(`/search/${this.state.clearInput}`);
-        })
+        this.props.history.push(`/search/${this.state.clearInput}`);
+      });
+  }
+  onLogout() {
+    this.props.logOut();
   }
 
-
-  /***** Para el rango de precio *****/
-  // onPriceSearch(event) {
-  //   event.preventDefault();
-  //   this.props.fetchPriceProperties(this.state.menor, this.state.mayor)
-  //   .then(() => {
-  //     this.props.history.push(`/price/${this.state.menor}/${this.state.mayor}`);
-  //   })
-  //   .then(() => this.setState({ menor: 0, mayor: 0 }))
-  // }
-  
-
   render() {
-    console.log(this.state);
-
     return (
       <Sidebar
         onSearch={this.onSearch}
         handleChange={this.handleChange}
+        user={this.props.user}
+        onLogout={this.onLogout}
       />
     );
   }
@@ -60,15 +60,18 @@ class SidebarContainer extends Component {
 const mapStateToProps = (state, ownProps) => {
   return {
     propiedadFiltrada: state.propiedadFiltrada,
+    user: state.user.logged.username,
     propiedades: state.propiedades.propiedades //borralo de ser necesaio
   };
 };
 
 const matchDispatchToProps = function(dispatch, ownprops) {
   return {
-    fetchFilteredProperties: (propiedadFiltrada, obj) => dispatch(fetchFilteredProperties(propiedadFiltrada, obj)),
+    fetchFilteredProperties: (propiedadFiltrada, obj) =>
+      dispatch(fetchFilteredProperties(propiedadFiltrada, obj)),
     // fetchPriceProperties: (menor, mayor) => dispatch(fetchPriceProperties(menor, mayor)),
-    fetchProducts: () => dispatch(fetchProducts()) //borralo de ser necesaio
+    fetchProducts: () => dispatch(fetchProducts()),
+    logOut: () => dispatch(logOutCreator())
   };
 };
 export default withRouter(
