77f6f8554339ba9aef8fe5092f159248606d7787
Rosanavpu <rosanap06@gmail.com>
2020-04-17 00:16:08 -0300

    proporciones, tamanos y errores resueltos.

--- front/src/containers/NavbarContainer.jsx
+++ front/src/containers/NavbarContainer.jsx
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
-import Navbar from "../components/Navbar";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom"
-import { logOutCreator } from "../redux/actions/getUser"
+// import React, { Component } from "react";
+// import Navbar from "../components/Navbar";
+// import { connect } from "react-redux";
+// import { withRouter } from "react-router-dom"
+// import { logOutCreator } from "../redux/actions/getUser"
 
 
-class NavbarContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.onLogout = this.onLogout.bind(this)
-  }
+// class NavbarContainer extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.onLogout = this.onLogout.bind(this)
+//   }
 
-  onLogout() {
-    this.props.logOut()
-  }
+//   onLogout() {
+//     this.props.logOut()
+//   }
 
-  render() {
-    return (
-      <Navbar user={this.props.user} onLogout={this.onLogout} />
-    );
-  }
-}
+//   render() {
+//     return (
+//       <Navbar user={this.props.user} onLogout={this.onLogout} />
+//     );
+//   }
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-   user: state.user.logged.username
-  };
-};
 
-const matchDispatchToProps = function (dispatch, ownprops) {
-  return {
-    logOut: () => dispatch(logOutCreator()),
-  }
-}
+// const mapStateToProps = (state, ownProps) => {
+//   return {
+//    user: state.user.logged.username
+//   };
+// };
 
-export default withRouter(connect(mapStateToProps, matchDispatchToProps)(NavbarContainer))
+// const matchDispatchToProps = function (dispatch, ownprops) {
+//   return {
+//     logOut: () => dispatch(logOutCreator()),
+//   }
+// }
+
+// export default withRouter(connect(mapStateToProps, matchDispatchToProps)(NavbarContainer))
